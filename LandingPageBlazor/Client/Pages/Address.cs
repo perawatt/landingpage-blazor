011c9b6288eef21c8b677a2205d42f8b71a69650
Sorry <so.sikarin@perfenterprise.com>
2021-07-15 13:39:51 +0700

    add address page

---
+++ LandingPageBlazor/Client/Pages/Address.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class AddressInfo
+{
+    public string Name { get; set; }
+    public string Add { get; set; }
+    public string Subdistrict { get; set; }
+    public string District { get; set; }
+    public string Provice { get; set; }
+    public string ZipCode { get; set; }
+    public string Tel { get; set; }
+    public string Email { get; set; }
+}
+
