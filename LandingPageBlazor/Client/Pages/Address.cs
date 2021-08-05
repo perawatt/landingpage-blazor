591097a9a5d957c386dae5c9c3d5e7b9980f179f
Sorry <so.sikarin@perfenterprise.com>
2021-08-05 15:11:41 +0700

    validate massage

--- LandingPageBlazor/Client/Pages/Address.cs
+++ LandingPageBlazor/Client/Pages/Address.cs
@@ -3,13 +3,28 @@ using System.ComponentModel.DataAnnotations;
 
 public class AddressInfo
 {
+    [Required(ErrorMessage = "ใส่ชื่อ นะจ๊ะ")]
     public string Name { get; set; }
+
+    [Required(ErrorMessage = "ที่อยู่ จ๊ะ")]
     public string Add { get; set; }
+
+    [Required]
     public string Subdistrict { get; set; }
+
+    [Required]
     public string District { get; set; }
+
+    [Required]
     public string Provice { get; set; }
+
+    [Required]
     public string ZipCode { get; set; }
+
+    [Required(ErrorMessage = "ใส่เบอมาสิ")]
     public string Tel { get; set; }
+
+    [Required(ErrorMessage = "อีเมล หยัง?")]
     public string Email { get; set; }
 }
 
